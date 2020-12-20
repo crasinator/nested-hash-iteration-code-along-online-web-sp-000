188daab7131c8cb7cd21f68aba6f7e8ddf222f2d
Jordan Reed <jordanreed03@gmail.com>
2020-12-20 18:21:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,11 @@ require 'pry'
 def remove_strawberry(contacts)
 contacts.each do |name, details|
  if name == "Freddy Mercury" 
-   details. each do |attribute, value|
+   details. each do |attribute, data|
+     if attribute == :favorite_ice_cream_flavors
+       data.delete_if {|flav| flav == "strawberry"}
 end 
 end
+end 
+end 
+end 
